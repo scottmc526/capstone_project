bd7286048eeae15e3cb0d945695d1ec72911160a
Scott Christensen <scottmc526@gmail.com>
2016-04-05 15:13:53 -0600

    scoring system

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,4 +1,69 @@
+Array.prototype.reverseForEach = function(callback){
+  //item, i reference callback
+  for(var i = this.length - 1; i >= 0; i--){
+    callback(this[i], i);
+  }
+}
+
+
 app.controller('mainController', function($scope){
-  $scope.key = document.getElementById('key')
-  console.log($scope.key);
+  $scope.frameOne = [null, null];
+  $scope.frameTwo = [null, null];
+  $scope.frameThree = [null, null];
+  $scope.allFrames = [];
+  $scope.allFrames.push($scope.frameOne, $scope.frameTwo, $scope.frameThree)
+
+
+
+  $scope.calculateScore = function(form) {
+
+    var tempArr = $scope.allFrames;
+    var score = 0;
+
+    tempArr.reverseForEach(function(frameArr, i){
+      var total = 0;
+      frameArr.forEach(function(item, i){
+        if(item === "x" || item === "/"){
+          total += 10;
+        }else{
+          total += +item;
+        }
+      })
+      score += total;
+    })
+
+    console.log(score);
+
+    // for (var i = tempArr.length - 1; i >= 0; i--) {
+    //   if(tempArr)
+    // }
+
+
+
+  }
+
+
 })
+
+
+// $scope.frameOneTotal = ''
+// var firstball = ''
+// var nextball = ''
+// var thirdball = ''
+//1st Frame
+// if ($scope.ballTwo.toLowerCase() == 'x') {
+//   $scope.frameOneTotal = "you cant have a strike there"
+// } else if ($scope.ballOne.toLowerCase() == '/'){
+//   $scope.frameOneTotal = 'you cant have a spare there'
+// } else if ($scope.ballOne.toLowerCase == 'x') {
+//   firstball = 10;
+//   nextball = $scope.ballThree;
+//   if ($scope.ballThree.toLowerCase == 'x') {
+//     thirdball = Number($scope.ballFive)
+//   } else {
+//     thirdball = Number($scope.ballFour)
+//   }
+// } else {
+//   firstball = Number($scope.ballOne)
+// }
+// console.log('first:', firstball, 'second:', nextball, 'third:', thirdball);
---
+++ public/js/services/mainService.js
@@ -0,0 +1,12 @@
+app.service('toScore', function(){
+  this.createArray = function(num) {
+    var arr = [];
+    arr.push(num);
+    return arr;
+  }
+
+
+
+
+
+})
