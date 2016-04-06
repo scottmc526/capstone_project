ba4360817cea510b6e01f94977b490cc34bb491e
Scott Christensen <scottmc526@gmail.com>
2016-04-05 18:40:28 -0600

    ideas

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -10,39 +10,42 @@ app.controller('mainController', function($scope){
   $scope.frameOne = [null, null];
   $scope.frameTwo = [null, null];
   $scope.frameThree = [null, null];
+  $scope.frameFour = [null, null];
   $scope.allFrames = [];
-  $scope.allFrames.push($scope.frameOne, $scope.frameTwo, $scope.frameThree)
+  $scope.allFrames.push($scope.frameOne, $scope.frameTwo, $scope.frameThree, $scope.frameFour)
 
+  $scope.allShots = [];
+
+
+
+  // $scope.calculateScore = function(form) {
+  //
+  //   var tempArr = $scope.allFrames;
+  //   $scope.frameOneTotal = false;
+  //   $scope.score = 0;
+  //
+  //   tempArr.reverseForEach(function(frameArr, i){
+  //     var total = 0;
+  //     frameArr.forEach(function(item, i){
+  //       if(item === "x"){
+  //         total += 10;
+  //       } else if (item === '/'){
+  //         total += (10 - frameArr[i-1])
+  //       } else {
+  //         total += +item;
+  //         $scope.frameOneTotal =true;
+  //       }
+  //     })
+  //     $scope.score += total;
+  //   })
+  // }
 
 
   $scope.calculateScore = function(form) {
-
     var tempArr = $scope.allFrames;
-    $scope.frameOneTotal = false;
-    $scope.score = 0;
-
-    tempArr.reverseForEach(function(frameArr, i){
-      var total = 0;
-      frameArr.forEach(function(item, i){
-        if(item === "x"){
-          total += 10;
-        } else if (item === '/'){
-          total += (10 - frameArr[i-1])
-        } else {
-          total += +item;
-          $scope.frameOneTotal =true;
-        }
-      })
-      $scope.score += total;
-    })
-
-    console.log($scope.score);
-
-
-
-
-
+    $scope.score = 0
+    for (var i = 0; i < tempArr.length; i++) {
+      console.log(tempArr[0]);
+    }
   }
-
-
 })
