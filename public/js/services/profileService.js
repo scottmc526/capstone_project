90ae10bc989e7ca3868390c478f7c018dcdd2a30
Scott Christensen <scottmc526@gmail.com>
2016-04-13 00:54:51 -0600

    profile_style

--- public/js/services/profileService.js
+++ public/js/services/profileService.js
@@ -1,8 +1,8 @@
 app.service('profileService', function($http) {
   var profileService = {}
   profileService.getScores = function() {
-    // return $http.get('http://localhost:3000/scores', {method: "jsonp"});
-    return $http.get('https://bowlingscore.herokuapp.com/scores', {method: 'jsonp'})
+    return $http.get('http://localhost:3000/scores', {method: "jsonp"});
+    // return $http.get('https://bowlingscore.herokuapp.com/scores', {method: 'jsonp'})
   }
   return profileService;
 })
