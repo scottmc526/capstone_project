39433e2203ae88efb75036443c3275efd60c1ed9
Scott Christensen <scottmc526@gmail.com>
2016-04-12 22:26:48 -0600

    http for heroku

--- public/js/services/profileService.js
+++ public/js/services/profileService.js
@@ -1,7 +1,8 @@
 app.service('profileService', function($http) {
   var profileService = {}
   profileService.getScores = function() {
-    return $http.get('http://localhost:3000/scores', {method: "jsonp"});
+    // return $http.get('http://localhost:3000/scores', {method: "jsonp"});
+    return $http.get('http://bowlingscore.herokuapp.com/scores', {method: 'jsonp'})
   }
   return profileService;
 })
